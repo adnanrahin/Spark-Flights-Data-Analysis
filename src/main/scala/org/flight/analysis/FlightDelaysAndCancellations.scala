6b8eeca67c7858c9150686a90e7703a5535f20e9
adnanrahin <adnan.rahin13@gmail.com>
2021-09-30 23:43:47 -0400

    max distance airports data frame created

--- src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
+++ src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
@@ -59,7 +59,7 @@ object FlightDelaysAndCancellations {
         findMostCancelledAirlineToDF(flightsRDD, airlineRDD, spark)
         findAverageDepartureDelayOfAirlinerToDF(flightsRDD, airlineRDD, spark)
         findTotalDistanceFlownEachAirlineToDF(flightsRDD, airlineRDD, spark)*/
-    findOriginAndDestinationByMaxDistance(flightsRDD, airportRDD)
+    findOriginAndDestinationByMaxDistanceToDF(flightsRDD, airportRDD, spark)
 
     spark.close()
 
@@ -262,20 +262,32 @@ object FlightDelaysAndCancellations {
 
   def findOriginAndDestinationByMaxDistance(flightsRDD: RDD[Flight], airportRDD: RDD[Airport]): (String, String, Long) = {
 
-    val airportNames: (String, String, Long) = flightsRDD
+    val airportNamesAndDistance: (String, String, Long) = flightsRDD
       .filter(flight => flight.diverted.equals("0"))
       .map(flight => (flight.originAirport, flight.destinationAirport, flight.distance.toLong))
       .max()(Ordering[Long].on(x => x._3))
 
     val airportPairRDD: RDD[(String, String)] =
       airportRDD
-        .filter(airport => airport.iataCode.equals(airportNames._1) || airport.iataCode.equals(airportNames._2))
+        .filter(airport => airport.iataCode.equals(airportNamesAndDistance._1) || airport.iataCode.equals(airportNamesAndDistance._2))
         .map(airport => (airport.iataCode, airport.airport))
 
-    val source: String = airportPairRDD.distinct().lookup(airportNames._1)(0)
-    val destination: String = airportPairRDD.distinct().lookup(airportNames._2)(0)
+    val source: String = airportPairRDD.distinct().lookup(airportNamesAndDistance._1)(0)
+    val destination: String = airportPairRDD.distinct().lookup(airportNamesAndDistance._2)(0)
 
-    (source, destination, airportNames._3)
+    (source, destination, airportNamesAndDistance._3)
+  }
+
+  def findOriginAndDestinationByMaxDistanceToDF(flightsRDD: RDD[Flight], airportRDD: RDD[Airport], spark: SparkSession): Unit = {
+
+    val airportNamesAndDistance: (String, String, Long) =
+      findOriginAndDestinationByMaxDistance(flightsRDD, airportRDD)
+
+    spark
+      .createDataFrame(List(airportNamesAndDistance))
+      .toDF("Source Airport", "Destination Airport", "Total Distance")
+      .show(5, truncate = false)
+
   }
 
   def findTotalDistanceFlownEachAirlineToDF(flightsRDD: RDD[Flight], airlineRDD: RDD[Airline], spark: SparkSession): Unit = {
